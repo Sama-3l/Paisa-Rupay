a88951978aa639de399f446a512fb8fd31559cee
Raghvendra Mishra <raghvendramishra2002@gmail.com>
2026-06-10 00:02:24 +0530

    Metadata updates

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -7,18 +7,6 @@ import TheProblem from './TheProblem/the_problem';
 import TheSolution from './TheSolution/the_solution';
 import { Metadata } from 'next';
 
-export const metadata: Metadata = {
-  icons: {
-    icon: [
-      { url: '/favicons/icon.png', type: 'image/png' },
-      { url: '/favicons/favicon.ico', sizes: '32x32' },
-    ],
-    apple: [{ url: '/favicons/apple-touch-icon.png', sizes: '180x180' }],
-    shortcut: '/favicons/favicon.ico',
-  },
-  manifest: '/favicons/manifest.webmanifest',
-}
-
 export default function HomePage() {
   return (
     <>
