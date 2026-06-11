451b600cd577471a2d318afb8fd5b512700310ae
Raghvendra Mishra <raghvendramishra2002@gmail.com>
2026-06-11 17:09:05 +0530

    Design updates to legal

--- app/legal/terms-of-use/TermsOfUseLayout.tsx
+++ app/legal/terms-of-use/TermsOfUseLayout.tsx
@@ -6,8 +6,8 @@ export default function TermsOfUseLayout({ children }: { children: React.ReactNo
   return (
     <div className='sm:pt-20 pt-12 sm:w-[90%] w-full sm:px-0 px-4 self-center sm:pb-40 pb-20'>
       <SectionHeading heading="Terms of Use" caption="LEGAL" />
-      <div className='bg-(--placeholder)/30 h-[0.5px] w-full mt-40'></div>
-      <div className='flex flex-row mt-15'>
+      <div className='bg-(--placeholder)/30 h-[0.5px] w-full sm:mt-40 mt-20'></div>
+      <div className='flex sm:flex-row flex-col sm:mt-15 mt-5'>
         <div className={styles.date_text}>Last Updated<br />June 11th, 2026</div>
         <div className={styles.prose}>
           {children}
